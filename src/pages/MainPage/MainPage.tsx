17537e66cdc32a2043aae6b42473e004c88c0855
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2023-01-19 21:24:01 +0900

    fix: 내용 추가, 틀 잡기

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import SideBar from "../../components/SideBar/SideBar";
-import { Container, MainDiv } from "./MainPageStyle";
+import { Container, MainDiv, Blue } from "./MainPageStyle";
 
 const MainPage = () => {
   return (
     <Container>
       <SideBar />
-      <MainDiv>main page</MainDiv>
+      <MainDiv>
+        알게 된 다양한 기술, 트렌드들을 동료와 나누며
+        <br />
+        좋은 개발 문화를 만들고 싶은 신입
+        <Blue> 프론트엔드 개발자</Blue> 이도연 입니다.
+      </MainDiv>
     </Container>
   );
 };
