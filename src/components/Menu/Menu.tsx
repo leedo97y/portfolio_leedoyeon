bdc7eefb6a71fffddb62300484603fbc23bdc972
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2023-01-19 19:37:45 +0900

    fix: 메뉴 컴포넌트로 빼기

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Menu, NavbarLink } from "./MenuStyle";
+
+const MenuComponent: React.FC = () => {
+  return (
+    <div>
+      <Menu>
+        <button>
+          <NavbarLink to="/">main</NavbarLink>
+        </button>
+        <button>
+          <NavbarLink to="/about">about me</NavbarLink>
+        </button>
+        <button>
+          <NavbarLink to="#">tech skills</NavbarLink>
+        </button>
+        <button>
+          <NavbarLink to="#">project</NavbarLink>
+        </button>
+      </Menu>
+    </div>
+  );
+};
+
+export default MenuComponent;
