34835cd930bb4df42c05f8093efcf70565afd7c1
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2023-01-19 21:22:14 +0900

    style: 프로젝트 페이지, 테크 페이지 스타일 틀 잡기

--- src/pages/TechSkillsPage/TechPageStyle.ts
+++ src/pages/TechSkillsPage/TechPageStyle.ts
@@ -1,1 +1,12 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 400px 1fr;
+`;
+
+export const TechDiv = styled.div`
+  height: 500px;
+  background-color: white;
+  padding: 50px;
+`;
