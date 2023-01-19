c5ecef705be5d417179030d26d0c79f966897cf2
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2023-01-19 22:00:03 +0900

    style: 정보 페이지 틀 잡기

--- src/pages/AboutPage/AboutPageStyle.ts
+++ src/pages/AboutPage/AboutPageStyle.ts
@@ -1,7 +1,30 @@
 import styled from "styled-components";
 
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 400px 1fr;
+`;
+
 export const AboutDiv = styled.div`
-  height: 100vh;
+  height: 400px;
   background-color: white;
   padding: 50px;
+  font-size: 20px;
+  letter-spacing: 1.3px;
+  line-height: 30px;
+
+  h1 {
+    font-weight: 400;
+  }
+
+  h3 {
+    font-weight: 400;
+  }
+
+  p {
+    font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS",
+      sans-serif;
+    font-weight: 400;
+    font-size: 15px;
+  }
 `;
