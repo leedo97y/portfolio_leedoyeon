9df650a148722b1cebd2345cedf61b957abec8d3
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2023-01-21 01:40:33 +0900

    feat: 계란후라이 프로젝트 내용 추가

---
+++ src/pages/ProjectPage/components/FriedEggSkill.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { WholeBtn, Button } from "./SkillButtonStyle";
+import HtmlLogo from "../../../assets/images/htmllogo.png";
+import CssLogo from "../../../assets/images/csslogo.png";
+import JsLogo from "../../../assets/images/jslogo.png";
+import EjsLogo from "../../../assets/images/ejslogo.png";
+import BulmaLogo from "../../../assets/images/bulmalogo.png";
+
+import NodeLogo from "../../../assets/images/nodejslogo.png";
+import MongodbLogo from "../../../assets/images/mongodblogo.png";
+import ExpressLogo from "../../../assets/images/expresslogo.png";
+import MongooseLogo from "../../../assets/images/mongooselogo.png";
+
+import GitLogo from "../../../assets/images/gitlogo.png";
+import FigmaLogo from "../../../assets/images/figmalogo.png";
+import TrelloLogo from "../../../assets/images/trellologo.jpeg";
+
+const FriedEggFrontSkill: React.FC = () => {
+  return (
+    <WholeBtn>
+      <Button>
+        <img src={HtmlLogo} alt="HTML" />
+      </Button>
+      <Button>
+        <img src={CssLogo} alt="CSS" />
+      </Button>
+      <Button>
+        <img src={JsLogo} alt="JavaScript" />
+      </Button>
+      <Button>
+        <img src={EjsLogo} alt="ejs" />
+      </Button>
+      <Button>
+        <img src={BulmaLogo} alt="bulma css" />
+      </Button>
+    </WholeBtn>
+  );
+};
+
+const FriedEggBackSkill: React.FC = () => {
+  return (
+    <WholeBtn>
+      <Button>
+        <img src={NodeLogo} alt="node js" />
+      </Button>
+      <Button>
+        <img src={MongodbLogo} alt="mongoDB" />
+      </Button>
+      <Button>
+        <img src={JsLogo} alt="JavaScript" />
+      </Button>
+      <Button>
+        <img src={ExpressLogo} alt="express js" />
+      </Button>
+      <Button>
+        <img src={MongooseLogo} alt="mongoose" />
+      </Button>
+    </WholeBtn>
+  );
+};
+
+const FriedEggTool: React.FC = () => {
+  return (
+    <WholeBtn>
+      <Button>
+        <img src={GitLogo} alt="git" />
+      </Button>
+      <Button>
+        <img src={TrelloLogo} alt="Trello" />
+      </Button>
+      <Button>
+        <img src={FigmaLogo} alt="figma" />
+      </Button>
+    </WholeBtn>
+  );
+};
+
+export { FriedEggFrontSkill, FriedEggBackSkill, FriedEggTool };
