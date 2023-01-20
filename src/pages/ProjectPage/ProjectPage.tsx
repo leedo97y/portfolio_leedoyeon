1892e18199457a4593f4c8cba9f7351e527313ba
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2023-01-21 01:42:46 +0900

    fix: 첫 페이지 내용 추가

--- src/pages/ProjectPage/ProjectPage.tsx
+++ src/pages/ProjectPage/ProjectPage.tsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import SideBar from "../../components/SideBar/SideBar";
-import { Container, ProjectDiv } from "../ProjectPage/ProjectPageStyle";
+import Weather from "./components/Weather";
+import { Container, ProjectDiv, Contents } from "./ProjectPageStyle";
+import FriedEgg from "./components/FriedEgg";
+import Retfilx from "./components/Retfilx";
+import TripMatch from "./components/TripMatch";
+// LayerDiv
 
-const ProjectPage = () => {
+const ProjectPage: React.FC = () => {
+  // const [click, setClick] = useState<boolean>(false);
+
+  // const onClickBtn: React.MouseEventHandler<HTMLButtonElement> = (e) => {
+  //   setClick(!click);
+  // };
+
   return (
     <div>
       <Container>
         <SideBar />
-        <ProjectDiv>HI</ProjectDiv>
+        <ProjectDiv>
+          <h1>project</h1>
+          <button>오늘 뭐 입지?</button>
+          <button>계란후라이</button>
+          <button>Retfilx</button>
+          <button>TripMatch</button>
+          {}
+          {/* <Contents>
+            <Weather />
+          </Contents> */}
+          <Contents>
+            <FriedEgg />
+          </Contents>
+          <Contents>
+            <Retfilx />
+          </Contents>
+          {/* <Contents>
+            <TripMatch />
+          </Contents> */}
+        </ProjectDiv>
       </Container>
     </div>
   );
