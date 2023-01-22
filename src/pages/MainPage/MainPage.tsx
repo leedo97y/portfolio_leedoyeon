9a58f0479b1b49d9dc4485588f38ba4fe5268a1b
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2023-01-22 16:07:44 +0900

    fix: 메인페이지 div 추가

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -6,10 +6,13 @@ const MainPage: React.FC = () => {
   return (
     <Container>
       <SideBar />
+
       <MainDiv>
-        <p>소통이 잘 되며,</p>
-        <p>알게 된 다양한 기술, 트렌드들을 동료와 나누며</p>
-        <p>좋은 개발 문화를 만들고 싶은 웹 개발자 이도연 입니다.</p>
+        <div>
+          <p>소통이 잘 되며,</p>
+          <p>알게 된 다양한 기술, 트렌드들을 동료와 나누며</p>
+          <p>좋은 개발 문화를 만들고 싶은 웹 개발자 이도연 입니다.</p>
+        </div>
       </MainDiv>
     </Container>
   );
